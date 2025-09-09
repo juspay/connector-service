cbb66b49acf5bc1c2dd21e64cdbc8b9688fd8668
Tushar Shukla <shuklatuhsar100@gmail.com>
2025-09-09 19:31:38 +0530

    fixes for derialization

--- backend/connector-integration/src/connectors/worldpay/response.rs
+++ backend/connector-integration/src/connectors/worldpay/response.rs
@@ -11,6 +11,10 @@ use super::requests::*;
 pub struct WorldpayPaymentsResponse {
     pub outcome: PaymentOutcome,
     pub transaction_reference: Option<String>,
+    #[serde(rename = "_links", skip_serializing_if = "Option::is_none")]
+    pub links: Option<SelfLink>,
+    #[serde(rename = "_actions", skip_serializing_if = "Option::is_none")]
+    pub actions: Option<ActionLinks>,
     #[serde(flatten)]
     pub other_fields: Option<WorldpayPaymentResponseFields>,
 }
@@ -33,10 +37,6 @@ pub struct AuthorizedResponse {
     pub issuer: Option<Issuer>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub scheme: Option<PaymentsResponseScheme>,
-    #[serde(rename = "_links", skip_serializing_if = "Option::is_none")]
-    pub links: Option<SelfLink>,
-    #[serde(rename = "_actions")]
-    pub actions: Option<ActionLinks>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
     pub risk_factors: Option<Vec<RiskFactorsInner>>,
@@ -271,25 +271,25 @@ pub fn get_resource_id<T, F>(
 where
     F: Fn(String) -> T,
 {
+    // First check top-level _links (for capture, authorize, etc.)
     let optional_reference_id = response
-        .other_fields
+        .links
         .as_ref()
-        .and_then(|other_fields| match other_fields {
-            WorldpayPaymentResponseFields::AuthorizedResponse(res) => res
-                .links
-                .as_ref()
-                .and_then(|link| link.self_link.href.rsplit_once('/').map(|(_, h)| h)),
-            WorldpayPaymentResponseFields::DDCResponse(res) => {
-                res.actions.supply_ddc_data.href.split('/').nth_back(1)
-            }
-            WorldpayPaymentResponseFields::ThreeDsChallenged(res) => res
-                .actions
-                .complete_three_ds_challenge
-                .href
-                .split('/')
-                .nth_back(1),
-            WorldpayPaymentResponseFields::FraudHighRisk(_)
-            | WorldpayPaymentResponseFields::RefusedResponse(_) => None,
+        .and_then(|link| link.self_link.href.rsplit_once('/').map(|(_, h)| h))
+        .or_else(|| {
+            // Fallback to variant-specific logic for DDC and 3DS challenges
+            response.other_fields.as_ref().and_then(|other_fields| match other_fields {
+                WorldpayPaymentResponseFields::DDCResponse(res) => {
+                    res.actions.supply_ddc_data.href.split('/').nth_back(1)
+                }
+                WorldpayPaymentResponseFields::ThreeDsChallenged(res) => res
+                    .actions
+                    .complete_three_ds_challenge
+                    .href
+                    .split('/')
+                    .nth_back(1),
+                _ => None,
+            })
         })
         .map(|href| {
             urlencoding::decode(href)
