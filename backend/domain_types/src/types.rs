91da9130004520f10b12f15c6f727254048d510f
yashasvi.kapil <yashasvi.kapil@juspay.in>
2025-09-16 19:09:29 +0530

    Auth, Capture,Psync ,Refund,Rsync,Void working

--- backend/domain_types/src/types.rs
+++ backend/domain_types/src/types.rs
@@ -130,6 +130,7 @@ pub struct ConnectorParams {
     /// base url
     pub base_url: String,
     pub dispute_base_url: Option<String>,
+    pub secondary_base_url: Option<String>,
 }
 
 #[derive(Debug, serde::Deserialize, Clone, Default)]
