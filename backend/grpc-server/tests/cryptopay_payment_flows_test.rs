49c3ba4e3e1ec2c699aded9bef009a509e7537ee
Saptak Dutta <saptakd88@gmail.com>
2025-09-21 20:27:56 +0530

    fix errors

--- backend/grpc-server/tests/cryptopay_payment_flows_test.rs
+++ backend/grpc-server/tests/cryptopay_payment_flows_test.rs
@@ -131,6 +131,8 @@ fn create_payment_sync_request(request_ref_id: &str) -> PaymentServiceGetRequest
         request_ref_id: Some(Identifier {
             id_type: Some(IdType::Id(request_ref_id.to_string())),
         }),
+        capture_method: None,
+        access_token: None,
     }
 }
 
