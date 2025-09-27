ae016fa38cf8113073bb9b5d6da716233641acd9
Tushar Shukla <shuklatuhsar100@gmail.com>
2025-09-28 02:46:27 +0530

    Compilation after rebase

--- backend/domain_types/src/types.rs
+++ backend/domain_types/src/types.rs
@@ -4903,6 +4903,19 @@ impl<
             + PartialEq
             + serde::Serialize
             + serde::de::DeserializeOwned
+        })
+    }
+}
+
+impl<
+        T: PaymentMethodDataTypes
+            + Default
+            + Debug
+            + Send
+            + Eq
+            + PartialEq
+            + serde::Serialize
+            + serde::de::DeserializeOwned
             + Clone
             + CardConversionHelper<T>,
     > ForeignTryFrom<grpc_api_types::payments::PaymentServiceAuthorizeRequest>
