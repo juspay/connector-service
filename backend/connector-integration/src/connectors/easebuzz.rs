a8b7e09582c5366dedc56d813916d2f763e96379
Piyush Kesharwani <piyush.k.001@juspay.in>
2025-10-10 12:03:10 +0530

    EUL-0000 Auto-commit changes from tool execution chatcmpl-tool-5da07519b9d34bd2a950d6c00ebde65f

---
+++ backend/connector-integration/src/connectors/easebuzz.rs
@@ -0,0 +1,523 @@
+mod test;
+pub mod transformers;
+use std::{
+    fmt::Debug,
+    marker::{Send, Sync},
+    sync::LazyLock,
+};
+
+use common_enums::{
+    AttemptStatus, Currency, PaymentMethod, PaymentMethodType,
+};
+use common_utils::{
+    errors::CustomResult, ext_traits::ByteSliceExt, pii::SecretSerdeValue, types::StringMinorUnit,
+};
+use domain_types::{
+    connector_flow::{
+        Authorize, PSync, RSync, Refund,
+    },
+    connector_types::{
+        ConnectorSpecifications, ConnectorWebhookSecrets, PaymentFlowData,
+        PaymentsAuthorizeData, PaymentsResponseData, PaymentsSyncData, RefundFlowData,
+        RefundSyncData, RefundWebhookDetailsResponse, RefundsData, RefundsResponseData,
+        RequestDetails, ResponseId, SupportedPaymentMethodsExt, WebhookDetailsResponse,
+    },
+    errors,
+    payment_method_data::{DefaultPCIHolder, PaymentMethodData, PaymentMethodDataTypes},
+    router_data::{ConnectorAuthType, ErrorResponse},
+    router_data_v2::RouterDataV2,
+    router_response_types::Response,
+    types::{
+        self, ConnectorInfo, Connectors, FeatureStatus,
+        PaymentMethodDataType, PaymentMethodDetails, PaymentMethodSpecificFeatures,
+        SupportedPaymentMethods,
+    },
+    utils,
+};
+use error_stack::report;
+use hyperswitch_masking::{Mask, Maskable};
+use interfaces::{
+    api::ConnectorCommon,
+    connector_integration_v2::ConnectorIntegrationV2,
+    connector_types::{self, is_mandate_supported, ConnectorValidation},
+    events::connector_api_logs::ConnectorEvent,
+};
+use serde::Serialize;
+use transformers::{
+    self as easebuzz, EaseBuzzInitiatePaymentRequest, EaseBuzzInitiatePaymentResponse,
+    EaseBuzzSeamlessTxnRequest, EaseBuzzSeamlessTxnResponse, EaseBuzzTxnSyncRequest,
+    EaseBuzzTxnSyncResponse, EaseBuzzRefundRequest, EaseBuzzRefundResponse,
+    EaseBuzzRefundSyncRequest, EaseBuzzRefundSyncResponse, EaseBuzzUpiIntentResponse,
+};
+
+use super::macros;
+use crate::{types::ResponseRouterData, with_error_response_body};
+
+pub(crate) mod headers {
+    pub(crate) const CONTENT_TYPE: &str = "Content-Type";
+    pub(crate) const X_API_KEY: &str = "X-Api-Key";
+}
+
+// Type alias for non-generic trait implementations
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    connector_types::ConnectorServiceTrait<T> for EaseBuzz<T>
+{
+}
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    connector_types::PaymentAuthorizeV2<T> for EaseBuzz<T>
+{
+}
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    connector_types::PaymentSyncV2 for EaseBuzz<T>
+{
+}
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    connector_types::RefundV2 for EaseBuzz<T>
+{
+}
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    connector_types::RefundSyncV2 for EaseBuzz<T>
+{
+}
+
+macros::create_all_prerequisites!(
+    connector_name: EaseBuzz,
+    generic_type: T,
+    api: [
+        (
+            flow: Authorize,
+            request_body: EaseBuzzSeamlessTxnRequest,
+            response_body: EaseBuzzUpiIntentResponse,
+            router_data: RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
+        ),
+        (
+            flow: PSync,
+            request_body: EaseBuzzTxnSyncRequest,
+            response_body: EaseBuzzTxnSyncResponse,
+            router_data: RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>,
+        ),
+        (
+            flow: Refund,
+            request_body: EaseBuzzRefundRequest,
+            response_body: EaseBuzzRefundResponse,
+            router_data: RouterDataV2<Refund, RefundFlowData, RefundsData, RefundsResponseData>,
+        ),
+        (
+            flow: RSync,
+            request_body: EaseBuzzRefundSyncRequest,
+            response_body: EaseBuzzRefundSyncResponse,
+            router_data: RouterDataV2<RSync, RefundFlowData, RefundSyncData, RefundsResponseData>,
+        )
+    ],
+    amount_converters: [
+        amount_converter_webhooks: StringMinorUnit
+    ],
+    member_functions: {
+        pub fn build_headers<F, FCD, Req, Res>(
+            &self,
+            req: &RouterDataV2<F, FCD, Req, Res>,
+        ) -> CustomResult<Vec<(String, Maskable<String>)>, errors::ConnectorError> {
+            let mut header = vec![(
+                headers::CONTENT_TYPE.to_string(),
+                "application/x-www-form-urlencoded".to_string().into(),
+            )];
+            let mut api_key = self.get_auth_header(&req.connector_auth_type)?;
+            header.append(&mut api_key);
+            Ok(header)
+        }
+
+        pub fn connector_base_url_payments<'a, F, Req, Res>(
+            &self,
+            req: &'a RouterDataV2<F, PaymentFlowData, Req, Res>,
+        ) -> &'a str {
+            if req.test_mode {
+                "https://testpay.easebuzz.in"
+            } else {
+                &req.resource_common_data.connectors.easebuzz.base_url
+            }
+        }
+
+        pub fn connector_base_url_refunds<'a, F, Req, Res>(
+            &self,
+            req: &'a RouterDataV2<F, RefundFlowData, Req, Res>,
+        ) -> &'a str {
+            if req.test_mode {
+                "https://testpay.easebuzz.in"
+            } else {
+                &req.resource_common_data.connectors.easebuzz.base_url
+            }
+        }
+    }
+);
+
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize> ConnectorCommon
+    for EaseBuzz<T>
+{
+    fn id(&self) -> &'static str {
+        "easebuzz"
+    }
+    fn get_currency_unit(&self) -> common_enums::CurrencyUnit {
+        common_enums::CurrencyUnit::Minor
+    }
+    fn get_auth_header(
+        &self,
+        auth_type: &ConnectorAuthType,
+    ) -> CustomResult<Vec<(String, Maskable<String>)>, errors::ConnectorError> {
+        let auth = easebuzz::EaseBuzzAuthType::try_from(auth_type)
+            .map_err(|_| errors::ConnectorError::FailedToObtainAuthType)?;
+        Ok(vec![(
+            headers::X_API_KEY.to_string(),
+            auth.api_key.into_masked(),
+        )])
+    }
+    fn base_url<'a>(&self, connectors: &'a Connectors) -> &'a str {
+        connectors.easebuzz.base_url.as_ref()
+    }
+
+    fn build_error_response(
+        &self,
+        res: Response,
+        event_builder: Option<&mut ConnectorEvent>,
+    ) -> CustomResult<ErrorResponse, errors::ConnectorError> {
+        let response: easebuzz::EaseBuzzErrorResponse = res
+            .response
+            .parse_struct("ErrorResponse")
+            .map_err(|_| errors::ConnectorError::ResponseDeserializationFailed)?;
+
+        with_error_response_body!(event_builder, response);
+
+        Ok(ErrorResponse {
+            status_code: res.status_code,
+            code: response.error_code,
+            message: response.message.to_owned(),
+            reason: Some(response.message),
+            attempt_status: None,
+            connector_transaction_id: response.transaction_id,
+            network_decline_code: None,
+            network_advice_code: None,
+            network_error_message: None,
+        })
+    }
+}
+
+macros::macro_connector_implementation!(
+    connector_default_implementations: [get_content_type, get_error_response_v2],
+    connector: EaseBuzz,
+    curl_request: FormData(EaseBuzzSeamlessTxnRequest),
+    curl_response: EaseBuzzUpiIntentResponse,
+    flow_name: Authorize,
+    resource_common_data: PaymentFlowData,
+    flow_request: PaymentsAuthorizeData<T>,
+    flow_response: PaymentsResponseData,
+    http_method: Post,
+    generic_type: T,
+    [PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize ],
+    other_functions: {
+        fn get_headers(
+            &self,
+            req: &RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
+        ) -> CustomResult<Vec<(String, Maskable<String>)>, errors::ConnectorError> {
+            self.build_headers(req)
+        }
+        fn get_url(
+            &self,
+            req: &RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
+        ) -> CustomResult<String, errors::ConnectorError> {
+            Ok(format!("{}/payment/initiateLink", self.connector_base_url_payments(req)))
+        }
+    }
+);
+
+macros::macro_connector_implementation!(
+    connector_default_implementations: [get_content_type, get_error_response_v2],
+    connector: EaseBuzz,
+    curl_request: FormData(EaseBuzzTxnSyncRequest),
+    curl_response: EaseBuzzTxnSyncResponse,
+    flow_name: PSync,
+    resource_common_data: PaymentFlowData,
+    flow_request: PaymentsSyncData,
+    flow_response: PaymentsResponseData,
+    http_method: Post,
+    generic_type: T,
+    [PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize ],
+    other_functions: {
+        fn get_headers(
+            &self,
+            req: &RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>,
+        ) -> CustomResult<Vec<(String, Maskable<String>)>, errors::ConnectorError> {
+            self.build_headers(req)
+        }
+        fn get_url(
+            &self,
+            req: &RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>,
+        ) -> CustomResult<String, errors::ConnectorError> {
+            Ok(format!("{}/transaction/v1/retrieve", self.connector_base_url_payments(req)))
+        }
+    }
+);
+
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    connector_types::ValidationTrait for EaseBuzz<T>
+{
+}
+
+// SourceVerification implementations for all flows
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    interfaces::verification::SourceVerification<
+        Authorize,
+        PaymentFlowData,
+        PaymentsAuthorizeData<T>,
+        PaymentsResponseData,
+    > for EaseBuzz<T>
+{
+}
+
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    interfaces::verification::SourceVerification<
+        PSync,
+        PaymentFlowData,
+        PaymentsSyncData,
+        PaymentsResponseData,
+    > for EaseBuzz<T>
+{
+}
+
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    interfaces::verification::SourceVerification<
+        Refund,
+        RefundFlowData,
+        RefundsData,
+        RefundsResponseData,
+    > for EaseBuzz<T>
+{
+}
+
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    interfaces::verification::SourceVerification<
+        RSync,
+        RefundFlowData,
+        RefundSyncData,
+        RefundsResponseData,
+    > for EaseBuzz<T>
+{
+}
+
+impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
+    connector_types::IncomingWebhook for EaseBuzz<T>
+{
+    fn get_event_type(
+        &self,
+        request: RequestDetails,
+        _connector_webhook_secret: Option<ConnectorWebhookSecrets>,
+        _connector_account_details: Option<ConnectorAuthType>,
+    ) -> Result<domain_types::connector_types::EventType, error_stack::Report<errors::ConnectorError>>
+    {
+        let webhook: easebuzz::EaseBuzzWebhookTypes =
+            transformers::get_webhook_object_from_body(request.body).map_err(|err| {
+                report!(errors::ConnectorError::WebhookBodyDecodingFailed)
+                    .attach_printable(format!("error while decoding webhook body {err}"))
+            })?;
+        Ok(transformers::get_easebuzz_webhook_event_type(webhook))
+    }
+
+    fn process_payment_webhook(
+        &self,
+        request: RequestDetails,
+        _connector_webhook_secret: Option<ConnectorWebhookSecrets>,
+        _connector_account_details: Option<ConnectorAuthType>,
+    ) -> Result<WebhookDetailsResponse, error_stack::Report<errors::ConnectorError>> {
+        let request_body_copy = request.body.clone();
+        let webhook: easebuzz::EaseBuzzWebhookTypes =
+            transformers::get_webhook_object_from_body(request.body).map_err(|err| {
+                report!(errors::ConnectorError::WebhookBodyDecodingFailed)
+                    .attach_printable(format!("error while decoding webhook body {err}"))
+            })?;
+        
+        let (resource_id, status, error_code, error_message) = transformers::get_easebuzz_payment_webhook_details(webhook)?;
+
+        Ok(WebhookDetailsResponse {
+            resource_id: Some(resource_id),
+            status,
+            connector_response_reference_id: None,
+            error_code,
+            mandate_reference: None,
+            error_message,
+            raw_connector_response: Some(String::from_utf8_lossy(&request_body_copy).to_string()),
+            status_code: 200,
+            response_headers: None,
+        })
+    }
+
+    fn process_refund_webhook(
+        &self,
+        request: RequestDetails,
+        _connector_webhook_secret: Option<ConnectorWebhookSecrets>,
+        _connector_account_details: Option<ConnectorAuthType>,
+    ) -> Result<
+        domain_types::connector_types::RefundWebhookDetailsResponse,
+        error_stack::Report<errors::ConnectorError>,
+    > {
+        let request_body_copy = request.body.clone();
+        let webhook: easebuzz::EaseBuzzWebhookTypes =
+            transformers::get_webhook_object_from_body(request.body).map_err(|err| {
+                report!(errors::ConnectorError::WebhookBodyDecodingFailed)
+                    .attach_printable(format!("error while decoding webhook body {err}"))
+            })?;
+
+        let (connector_refund_id, status, error_code, error_message) = transformers::get_easebuzz_refund_webhook_details(webhook)?;
+
+        Ok(RefundWebhookDetailsResponse {
+            connector_refund_id: Some(connector_refund_id),
+            status,
+            connector_response_reference_id: None,
+            error_code,
+            error_message,
+            raw_connector_response: Some(String::from_utf8_lossy(&request_body_copy).to_string()),
+            status_code: 200,
+            response_headers: None,
+        })
+    }
+}
+
+macros::macro_connector_implementation!(
+    connector_default_implementations: [get_content_type, get_error_response_v2],
+    connector: EaseBuzz,
+    curl_request: FormData(EaseBuzzRefundRequest),
+    curl_response: EaseBuzzRefundResponse,
+    flow_name: Refund,
+    resource_common_data: RefundFlowData,
+    flow_request: RefundsData,
+    flow_response: RefundsResponseData,
+    http_method: Post,
+    generic_type: T,
+    [PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize],
+    other_functions: {
+        fn get_headers(
+            &self,
+            req: &RouterDataV2<Refund, RefundFlowData, RefundsData, RefundsResponseData>,
+        ) -> CustomResult<Vec<(String, Maskable<String>)>, errors::ConnectorError> {
+            self.build_headers(req)
+        }
+        fn get_url(
+            &self,
+            req: &RouterDataV2<Refund, RefundFlowData, RefundsData, RefundsResponseData>,
+        ) -> CustomResult<String, errors::ConnectorError> {
+            Ok(format!("{}/transaction/v1/refund", self.connector_base_url_refunds(req)))
+        }
+    }
+);
+
+macros::macro_connector_implementation!(
+    connector_default_implementations: [get_content_type, get_error_response_v2],
+    connector: EaseBuzz,
+    curl_request: FormData(EaseBuzzRefundSyncRequest),
+    curl_response: EaseBuzzRefundSyncResponse,
+    flow_name: RSync,
+    resource_common_data: RefundFlowData,
+    flow_request: RefundSyncData,
+    flow_response: RefundsResponseData,
+    http_method: Post,
+    generic_type: T,
+    [PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize],
+    other_functions: {
+        fn get_headers(
+            &self,
+            req: &RouterDataV2<RSync, RefundFlowData, RefundSyncData, RefundsResponseData>,
+        ) -> CustomResult<Vec<(String, Maskable<String>)>, errors::ConnectorError> {
+            self.build_headers(req)
+        }
+        fn get_url(
+            &self,
+            req: &RouterDataV2<RSync, RefundFlowData, RefundSyncData, RefundsResponseData>,
+        ) -> CustomResult<String, errors::ConnectorError> {
+            Ok(format!("{}/transaction/v1/refundStatus", self.connector_base_url_refunds(req)))
+        }
+    }
+);
+
+static EASEBUZZ_SUPPORTED_PAYMENT_METHODS: LazyLock<SupportedPaymentMethods> = LazyLock::new(|| {
+    let easebuzz_supported_capture_methods = vec![
+        CaptureMethod::Automatic,
+        CaptureMethod::Manual,
+    ];
+
+    let mut easebuzz_supported_payment_methods = SupportedPaymentMethods::new();
+
+    // UPI Intent support
+    easebuzz_supported_payment_methods.add(
+        PaymentMethod::Upi,
+        PaymentMethodType::UpiIntent,
+        PaymentMethodDetails {
+            mandates: FeatureStatus::NotSupported,
+            refunds: FeatureStatus::Supported,
+            supported_capture_methods: easebuzz_supported_capture_methods.clone(),
+            specific_features: None,
+        },
+    );
+
+    // UPI Collect support
+    easebuzz_supported_payment_methods.add(
+        PaymentMethod::Upi,
+        PaymentMethodType::UpiCollect,
+        PaymentMethodDetails {
+            mandates: FeatureStatus::NotSupported,
+            refunds: FeatureStatus::Supported,
+            supported_capture_methods: easebuzz_supported_capture_methods.clone(),
+            specific_features: None,
+        },
+    );
+
+    easebuzz_supported_payment_methods
+});
+
+static EASEBUZZ_CONNECTOR_INFO: ConnectorInfo = ConnectorInfo {
+    display_name: "EaseBuzz", 
+    description: "EaseBuzz is a payment gateway that provides UPI payment processing and other payment solutions for businesses in India.",
+    connector_type: types::PaymentConnectorCategory::PaymentGateway,
+};
+
+static EASEBUZZ_SUPPORTED_WEBHOOK_FLOWS: &[EventClass] = &[EventClass::Payments, EventClass::Refunds];
+
+impl ConnectorSpecifications for EaseBuzz<DefaultPCIHolder> {
+    fn get_connector_about(&self) -> Option<&'static ConnectorInfo> {
+        Some(&EASEBUZZ_CONNECTOR_INFO)
+    }
+
+    fn get_supported_payment_methods(&self) -> Option<&'static SupportedPaymentMethods> {
+        Some(&EASEBUZZ_SUPPORTED_PAYMENT_METHODS)
+    }
+
+    fn get_supported_webhook_flows(&self) -> Option<&'static [EventClass]> {
+        Some(EASEBUZZ_SUPPORTED_WEBHOOK_FLOWS)
+    }
+}
+
+impl ConnectorValidation for EaseBuzz<DefaultPCIHolder> {
+    fn validate_mandate_payment(
+        &self,
+        _pm_type: Option<PaymentMethodType>,
+        _pm_data: PaymentMethodData<DefaultPCIHolder>,
+    ) -> CustomResult<(), errors::ConnectorError> {
+        // EaseBuzz UPI doesn't support mandates
+        Err(errors::ConnectorError::MandateNotSupported)
+    }
+
+    fn validate_psync_reference_id(
+        &self,
+        data: &PaymentsSyncData,
+        _is_three_ds: bool,
+        _status: AttemptStatus,
+        _connector_meta_data: Option<SecretSerdeValue>,
+    ) -> CustomResult<(), errors::ConnectorError> {
+        if data.connector_transaction_id.is_some() {
+            return Ok(());
+        }
+        Err(errors::ConnectorError::MissingRequiredField {
+            field_name: "connector_transaction_id",
+        }
+        .into())
+    }
+    fn is_webhook_source_verification_mandatory(&self) -> bool {
+        false
+    }
+}
