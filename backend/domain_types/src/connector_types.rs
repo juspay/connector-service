940ac7427428f3e8ba89747baa55846bd0cd7745
Piyush Kesharwani <piyush.k.001@juspay.in>
2025-10-17 13:21:12 +0530

    ConnectorService EaseBuzz implementation - 2025-10-17T07:51:12.268Z
    Auto-commit changes from tool execution chatcmpl-tool-22039c7f4bc44554ae33cc627d9a44f4

--- backend/domain_types/src/connector_types.rs
+++ backend/domain_types/src/connector_types.rs
@@ -111,7 +111,7 @@ impl ForeignTryFrom<grpc_api_types::payments::Connector> for ConnectorEnum {
             grpc_api_types::payments::Connector::Trustpay => Ok(Self::Trustpay),
             grpc_api_types::payments::Connector::Stripe => Ok(Self::Stripe),
             grpc_api_types::payments::Connector::Cybersource => Ok(Self::Cybersource),
-            grpc_api_types::payments::Connector::EaseBuzz => Ok(Self::EaseBuzz),
+            grpc_api_types::payments::Connector::EASEBUZZ => Ok(Self::EaseBuzz),
             grpc_api_types::payments::Connector::Unspecified => {
                 Err(ApplicationErrorResponse::BadRequest(ApiError {
                     sub_code: "UNSPECIFIED_CONNECTOR".to_owned(),
