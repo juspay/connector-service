2c6f919ab7592ef1e843e4e1367f5b45cb03530d
Piyush Kesharwani <piyush.k.001@juspay.in>
2025-10-28 16:22:36 +0530

    ConnectorService TPSL implementation - 2025-10-28T10:52:36.054Z
    Auto-commit changes from tool execution chatcmpl-tool-bb8a826f5805400c91a78b2d3f3d3ee3

--- backend/connector-integration/src/connectors/tpsl.rs
+++ backend/connector-integration/src/connectors/tpsl.rs
@@ -4,10 +4,21 @@ pub mod constants;
 use common_enums::CurrencyUnit;
 use common_utils::{errors::CustomResult, ext_traits::ByteSliceExt, types::StringMinorUnit};
 use domain_types::{
-    connector_flow::{Authorize, PSync},
+    connector_flow::{
+        Accept, Authenticate, Authorize, Capture, CreateAccessToken, CreateConnectorCustomer, 
+        CreateOrder, CreateSessionToken, DefendDispute, PaymentMethodToken, PostAuthenticate, 
+        PreAuthenticate, PSync, Refund, RepeatPayment, RSync, SetupMandate, SubmitEvidence, Void
+    },
     connector_types::{
-        ConnectorWebhookSecrets, PaymentFlowData, PaymentsAuthorizeData, PaymentsResponseData,
-        PaymentsSyncData, RequestDetails,
+        AcceptDisputeData, AccessTokenRequestData, AccessTokenResponseData, ConnectorCustomerData, 
+        ConnectorCustomerResponse, ConnectorWebhookSecrets, DisputeDefendData, DisputeFlowData, 
+        DisputeResponseData, PaymentCreateOrderData, PaymentCreateOrderResponse, PaymentFlowData, 
+        PaymentMethodTokenResponse, PaymentMethodTokenizationData, PaymentVoidData, 
+        PaymentsAuthenticateData, PaymentsAuthorizeData, PaymentsCaptureData, 
+        PaymentsPostAuthenticateData, PaymentsPreAuthenticateData, PaymentsResponseData, 
+        PaymentsSyncData, RefundFlowData, RefundSyncData, RefundsData, RefundsResponseData, 
+        RepeatPaymentData, RequestDetails, ResponseId, SessionTokenRequestData, SessionTokenResponseData, 
+        SetupMandateRequestData, SubmitEvidenceData
     },
     errors,
     payment_method_data::PaymentMethodDataTypes,
