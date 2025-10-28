59775b8c51db48c394860d3efe0df7c14a631192
Piyush Kesharwani <piyush.k.001@juspay.in>
2025-10-29 00:00:30 +0530

    ConnectorService Billdesk implementation - 2025-10-28T18:30:30.031Z
    Auto-commit changes from tool execution chatcmpl-tool-c7fb3e82b98c4d4d81babfa6d3a858dd

---
+++ backend/connector-integration/src/connectors/billdesk/constants.rs
@@ -0,0 +1,10 @@
+pub const BILLDESK_UAT_BASE_URL: &str = "https://uat.billdesk.com/pgidsk/PGIDirectRequest";
+pub const BILLDESK_PROD_BASE_URL: &str = "https://www.billdesk.com/pgidsk/PGIDirectRequest";
+
+pub const BILLDESK_UPI_REQUEST_ID: &str = "BDRDF011";
+pub const BILLDESK_NB_REQUEST_ID: &str = "BDRDF002";
+pub const BILLDESK_AUTH_REQUEST_ID: &str = "BDRDF002";
+
+pub const BILLDESK_SUCCESS_STATUS: &str = "0300";
+pub const BILLDESK_PENDING_STATUS: &str = "0002";
+pub const BILLDESK_FAILURE_STATUS: &str = "0399";
