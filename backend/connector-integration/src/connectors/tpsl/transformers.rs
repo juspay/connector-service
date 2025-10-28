af64089e44280117b3e4c0c5e62b39a90589b073
Piyush Kesharwani <piyush.k.001@juspay.in>
2025-10-28 14:55:49 +0530

    ConnectorService TPSL implementation - 2025-10-28T09:25:49.085Z
    Auto-commit changes from tool execution chatcmpl-tool-97d5e506d1e2461a88220d7f17ec727c

--- backend/connector-integration/src/connectors/tpsl/transformers.rs
+++ backend/connector-integration/src/connectors/tpsl/transformers.rs
@@ -467,7 +467,7 @@ impl<
     type Error = error_stack::Report<ConnectorError>;
 
     fn try_from(
-        item: TpslRouterData<RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>, T>,
+        item: super::TPSLRouterData<RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>, T>,
     ) -> Result<Self, Self::Error> {
         let auth = TpslAuthType::try_from(&item.router_data.connector_auth_type)?;
         let customer_id = item.router_data.resource_common_data.get_customer_id()?;
