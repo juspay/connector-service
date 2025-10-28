fdba83e0a4fd2864aa5eb97c33eb93f72e544119
Piyush Kesharwani <piyush.k.001@juspay.in>
2025-10-28 15:08:33 +0530

    ConnectorService TPSL implementation - 2025-10-28T09:38:32.932Z
    Auto-commit changes from tool execution chatcmpl-tool-d20cd07107f942399b0419cca89eb36b

--- backend/connector-integration/src/connectors/tpsl/transformers.rs
+++ backend/connector-integration/src/connectors/tpsl/transformers.rs
@@ -737,15 +737,7 @@ impl<
     }
 }
 
-impl<
-    T: PaymentMethodDataTypes
-        + std::fmt::Debug
-        + std::marker::Sync
-        + std::marker::Send
-        + 'static
-        + serde::Serialize
-        + serde::de::DeserializeOwned,
-> TryFrom<ResponseRouterData<TpslPaymentsResponse, RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>>>
+impl TryFrom<ResponseRouterData<TpslPaymentsResponse, RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>>>
     for RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>
 {
     type Error = error_stack::Report<ConnectorError>;
