d4b3aa5341cf3de0f2a2fb5d63d900366ccbf5df
Piyush Kesharwani <piyush.k.001@juspay.in>
2025-10-30 03:29:28 +0530

    ConnectorService TPSL implementation - 2025-10-29T21:59:27.854Z
    Auto-commit changes from tool execution chatcmpl-tool-50fa25ae2f20485aa1c744e5aa738319

--- backend/connector-integration/src/connectors/tpsl.rs
+++ backend/connector-integration/src/connectors/tpsl.rs
@@ -367,7 +367,7 @@ macros::create_all_prerequisites!(
     api: [
         (
             flow: Authorize,
-            request_body: TpslUPITxnRequest,
+            request_body: tpsl::TpslUPITxnRequest,
             response_body: TpslPaymentsResponse,
             router_data: RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
         ),
