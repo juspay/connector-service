9fbdd4f766a79e717c17cc129ecf53f1f7c1088c
Piyush Kesharwani <piyush.k.001@juspay.in>
2025-10-30 11:28:05 +0530

    ConnectorService TPSL implementation - 2025-10-30T05:58:05.206Z
    Auto-commit changes from tool execution call_4e061c0eea324ff894433f5b

--- backend/connector-integration/src/connectors/tpsl/mod.rs
+++ backend/connector-integration/src/connectors/tpsl/mod.rs
@@ -166,7 +166,7 @@ macro_rules! impl_not_implemented_flow {
 // impl_not_implemented_flow!(RepeatPayment, PaymentFlowData, RepeatPaymentData, PaymentsResponseData);
 // impl_not_implemented_flow!(Accept, DisputeFlowData, AcceptDisputeData, DisputeResponseData);
 // impl_not_implemented_flow!(DefendDispute, DisputeFlowData, DisputeDefendData, DisputeResponseData);
-impl_not_implemented_flow!(SubmitEvidence, DisputeFlowData, SubmitEvidenceData, DisputeResponseData);
+// impl_not_implemented_flow!(SubmitEvidence, DisputeFlowData, SubmitEvidenceData, DisputeResponseData);
 
 // Implement connector types traits - simplified for compilation
 
