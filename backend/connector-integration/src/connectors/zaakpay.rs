698740769e5f1c308c6e99454693e9a1e7977db7
Piyush Kesharwani <piyush.k.001@juspay.in>
2025-11-02 17:24:33 +0530

    ConnectorService ZaakPay implementation - 2025-11-02T11:54:33.448Z
    Auto-commit changes from tool execution chatcmpl-tool-dc18048d529745cdb32b70ab5b4bc746

--- backend/connector-integration/src/connectors/zaakpay.rs
+++ backend/connector-integration/src/connectors/zaakpay.rs
@@ -1075,6 +1075,46 @@ impl<
         Ok(payload.to_owned())
     }
 }
+impl<
+    T: PaymentMethodDataTypes
+        + std::fmt::Debug
+        + std::marker::Sync
+        + std::marker::Send
+        + 'static
+        + Serialize,
+> SourceVerification<domain_types::connector_flow::DefendDispute, DisputeFlowData, DisputeDefendData, DisputeResponseData> for ZaakPay<T>
+{
+    fn get_secrets(
+        &self,
+        _secrets: ConnectorSourceVerificationSecrets,
+    ) -> CustomResult<Vec<u8>, errors::ConnectorError> {
+        Ok(Vec::new())
+    }
+    fn get_algorithm(
+        &self,
+    ) -> CustomResult<
+        Box<dyn common_utils::crypto::VerifySignature + Send>,
+        errors::ConnectorError,
+    > {
+        Ok(Box::new(common_utils::crypto::NoAlgorithm))
+    }
+    fn get_signature(
+        &self,
+        _payload: &[u8],
+        _router_data: &RouterDataV2<domain_types::connector_flow::DefendDispute, DisputeFlowData, DisputeDefendData, DisputeResponseData>,
+        _secrets: &[u8],
+    ) -> CustomResult<Vec<u8>, errors::ConnectorError> {
+        Ok(Vec::new())
+    }
+    fn get_message(
+        &self,
+        payload: &[u8],
+        _router_data: &RouterDataV2<domain_types::connector_flow::DefendDispute, DisputeFlowData, DisputeDefendData, DisputeResponseData>,
+        _secrets: &[u8],
+    ) -> CustomResult<Vec<u8>, errors::ConnectorError> {
+        Ok(payload.to_owned())
+    }
+}
 
 
 
