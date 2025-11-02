1e2a6428aad502b2515cbc357912920a4d423519
Piyush Kesharwani <piyush.k.001@juspay.in>
2025-11-02 17:26:07 +0530

    ConnectorService ZaakPay implementation - 2025-11-02T11:56:06.747Z
    Auto-commit changes from tool execution chatcmpl-tool-050074925f8d4b08a8899a3bed53cdb0

--- backend/connector-integration/src/connectors/zaakpay.rs
+++ backend/connector-integration/src/connectors/zaakpay.rs
@@ -8,9 +8,8 @@ use common_utils::{
     errors::CustomResult, ext_traits::ByteSliceExt, types::StringMinorUnit,
 };
 use domain_types::{
-    connector_flow,
     connector_flow::{
-        Accept, Authorize, Capture, CreateOrder, CreateSessionToken, DefendDispute, PSync, RSync,
+        Accept, Authorize, Authenticate, CreateAccessToken, CreateConnectorCustomer, CreateOrder, CreateSessionToken, DefendDispute, PaymentMethodToken, PostAuthenticate, PreAuthenticate, PSync, RSync,
         Refund, RepeatPayment, SetupMandate, SubmitEvidence, Void, VoidPC,
     },
     connector_types::{
