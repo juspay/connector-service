bbe21574d0b53d575e20f70051b44050007c7fca
PiX <69745008+pixincreate@users.noreply.github.com>
2025-11-05 12:57:12 +0530

    initial commit

--- backend/domain_types/src/types.rs
+++ backend/domain_types/src/types.rs
@@ -117,6 +117,7 @@ pub struct Connectors {
     pub cybersource: ConnectorParams,
     pub worldpay: ConnectorParams,
     pub worldpayvantiv: ConnectorParams,
+    pub payload: ConnectorParams,
 }
 
 #[derive(Clone, serde::Deserialize, Debug, Default)]
