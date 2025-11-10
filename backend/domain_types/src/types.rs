e6ab8ee9c3e7c280cb0ddb3a86c95b64be26df7b
Simran Ray <johndoe@Simran-Ray-CH96RHFDQR.com>
2025-11-10 12:46:43 +0530

    bankofamerica-integration-Authorize-Capture-Psync-SetupMandate-Void

--- backend/domain_types/src/types.rs
+++ backend/domain_types/src/types.rs
@@ -147,6 +147,7 @@ pub struct Connectors {
     pub hipay: ConnectorParams,
     pub trustpayments: ConnectorParams,
     pub globalpay: ConnectorParams,
+    pub bankofamerica: ConnectorParams,
 }
 
 #[derive(Clone, Deserialize, Serialize, Debug, Default)]
