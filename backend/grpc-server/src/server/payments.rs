49783c424d81abffc4bd1d46ad1148bc45fd5d11
Tushar Shukla <shuklatuhsar100@gmail.com>
2025-11-12 00:05:35 +0530

    clippy fixes

--- backend/grpc-server/src/server/payments.rs
+++ backend/grpc-server/src/server/payments.rs
@@ -817,7 +817,7 @@ impl Payments {
         };
 
         // Execute connector processing
-        let response = external_services::service::execute_connector_processing_step(
+        let response = Box::pin(external_services::service::execute_connector_processing_step(
             &self.config.proxy,
             connector_integration,
             session_token_router_data,
@@ -825,7 +825,7 @@ impl Payments {
             external_event_params,
             None,
             common_enums::CallConnectorAction::Trigger,
-        )
+        ))
         .await
         .switch()
         .map_err(|e: error_stack::Report<ApplicationErrorResponse>| {
@@ -932,7 +932,7 @@ impl Payments {
             shadow_mode: event_params.shadow_mode,
         };
 
-        let response = external_services::service::execute_connector_processing_step(
+        let response = Box::pin(external_services::service::execute_connector_processing_step(
             &self.config.proxy,
             connector_integration,
             access_token_router_data,
@@ -940,7 +940,7 @@ impl Payments {
             external_event_params,
             None,
             common_enums::CallConnectorAction::Trigger,
-        )
+        ))
         .await
         .switch()
         .map_err(|e: error_stack::Report<ApplicationErrorResponse>| {
