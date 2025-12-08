1a08c2f1f2e4839199f1f6a6f6918cb8b92e00bf
gopikrishna.c <gopikrishna.c@juspay.in>
2025-12-08 11:51:36 +0530

    refactor: import

--- backend/connector-integration/src/connectors/paypal.rs
+++ backend/connector-integration/src/connectors/paypal.rs
@@ -660,7 +660,7 @@ macros::macro_connector_implementation!(
     connector_default_implementations: [get_content_type, get_error_response_v2],
     connector: Paypal,
     curl_request: Json(PaypalRefundRequest),
-    curl_response: paypal::RefundResponse,
+    curl_response: RefundResponse,
     flow_name: Refund,
     resource_common_data: RefundFlowData,
     flow_request: RefundsData,
@@ -697,7 +697,7 @@ macros::macro_connector_implementation!(
 macros::macro_connector_implementation!(
     connector_default_implementations: [get_content_type, get_error_response_v2],
     connector: Paypal,
-    curl_response: paypal::RefundSyncResponse,
+    curl_response: RefundSyncResponse,
     flow_name: RSync,
     resource_common_data: RefundFlowData,
     flow_request: RefundSyncData,
