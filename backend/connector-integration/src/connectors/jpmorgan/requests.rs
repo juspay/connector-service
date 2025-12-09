171db6d7b9e92207e80dd84ee91403530156d8a4
PiX <69745008+pixincreate@users.noreply.github.com>
2025-12-09 23:18:57 +0530

    feat(connector): jpmorgan

---
+++ backend/connector-integration/src/connectors/jpmorgan/requests.rs
@@ -0,0 +1,130 @@
+use std::str::FromStr;
+
+use common_utils::types::MinorUnit;
+use domain_types::{errors, payment_method_data::{PaymentMethodDataTypes, RawCardNumber}};
+use error_stack::report;
+use hyperswitch_masking::Secret;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize)]
+pub struct JpmorganTokenRequest {
+    pub grant_type: String,
+    pub scope: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JpmorganPaymentsRequest<T: PaymentMethodDataTypes> {
+    pub capture_method: CapMethod,
+    pub amount: MinorUnit,
+    pub currency: common_enums::Currency,
+    pub merchant: JpmorganMerchant,
+    pub payment_method_type: JpmorganPaymentMethodType<T>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JpmorganPaymentMethodType<T: PaymentMethodDataTypes> {
+    pub card: JpmorganCard<T>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JpmorganCard<T: PaymentMethodDataTypes> {
+    pub account_number: RawCardNumber<T>,
+    pub expiry: Expiry,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Expiry {
+    pub month: Secret<i32>,
+    pub year: Secret<i32>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JpmorganMerchant {
+    pub merchant_software: JpmorganMerchantSoftware,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JpmorganMerchantSoftware {
+    pub company_name: Secret<String>,
+    pub product_name: Secret<String>,
+}
+
+#[derive(Debug, Default, Copy, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum CapMethod {
+    #[default]
+    Now,
+    Delayed,
+    Manual,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JpmorganCaptureRequest {
+    pub capture_method: Option<CapMethod>,
+    pub amount: MinorUnit,
+    pub currency: Option<common_enums::Currency>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ReversalReason {
+    NoResponse,
+    LateResponse,
+    UnableToDeliver,
+    CardDeclined,
+    MacNotVerified,
+    MacSyncError,
+    ZekSyncError,
+    SystemMalfunction,
+    SuspectedFraud,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JpmorganVoidRequest {
+    pub amount: Option<MinorUnit>,
+    pub is_void: Option<bool>,
+    pub reversal_reason: Option<ReversalReason>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JpmorganRefundRequest {
+    pub merchant: JpmorganMerchantRefund,
+    pub amount: MinorUnit,
+    pub currency: common_enums::Currency,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JpmorganMerchantRefund {
+    pub merchant_software: JpmorganMerchantSoftware,
+}
+
+impl FromStr for ReversalReason {
+    type Err = error_stack::Report<errors::ConnectorError>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_uppercase().as_str() {
+            "NO_RESPONSE" => Ok(Self::NoResponse),
+            "LATE_RESPONSE" => Ok(Self::LateResponse),
+            "UNABLE_TO_DELIVER" => Ok(Self::UnableToDeliver),
+            "CARD_DECLINED" => Ok(Self::CardDeclined),
+            "MAC_NOT_VERIFIED" => Ok(Self::MacNotVerified),
+            "MAC_SYNC_ERROR" => Ok(Self::MacSyncError),
+            "ZEK_SYNC_ERROR" => Ok(Self::ZekSyncError),
+            "SYSTEM_MALFUNCTION" => Ok(Self::SystemMalfunction),
+            "SUSPECTED_FRAUD" => Ok(Self::SuspectedFraud),
+            _ => Err(report!(errors::ConnectorError::InvalidDataFormat {
+                field_name: "cancellation_reason",
+            })),
+        }
+    }
+}
---
+++ backend/connector-integration/src/connectors/jpmorgan/transformers.rs
@@ -0,0 +1,407 @@
+use common_enums::{AttemptStatus, CaptureMethod};
+use domain_types::{
+    connector_flow::{Authorize, Capture, CreateAccessToken, Refund, Void},
+    connector_types::{
+        AccessTokenRequestData, AccessTokenResponseData, PaymentFlowData, PaymentVoidData,
+        PaymentsAuthorizeData, PaymentsCaptureData, PaymentsResponseData, PaymentsSyncData,
+        RefundFlowData, RefundSyncData, RefundsData, RefundsResponseData, ResponseId,
+    },
+    errors,
+    payment_method_data::{PaymentMethodData, PaymentMethodDataTypes},
+    router_data::ConnectorAuthType,
+    router_data_v2::RouterDataV2,
+};
+use error_stack::ResultExt;
+use hyperswitch_masking::{PeekInterface, Secret};
+use serde::{Deserialize, Serialize};
+
+use super::{requests, responses};
+use crate::{connectors::jpmorgan::JpmorganRouterData, types::ResponseRouterData};
+
+type Error = error_stack::Report<errors::ConnectorError>;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct JpmorganAuthType {
+    pub client_id: Secret<String>,
+    pub client_secret: Secret<String>,
+}
+
+impl TryFrom<&ConnectorAuthType> for JpmorganAuthType {
+    type Error = error_stack::Report<errors::ConnectorError>;
+    fn try_from(auth_type: &ConnectorAuthType) -> Result<Self, Self::Error> {
+        match auth_type {
+            ConnectorAuthType::BodyKey {
+                api_key,
+                key1,
+            } => Ok(Self {
+                client_id: api_key.clone(),
+                client_secret: key1.clone(),
+            }),
+            _ => Err(errors::ConnectorError::FailedToObtainAuthType.into()),
+        }
+    }
+}
+
+// OAuth 2.0 transformers
+impl<T: PaymentMethodDataTypes + std::fmt::Debug + std::marker::Sync + std::marker::Send + 'static + Serialize> TryFrom<JpmorganRouterData<RouterDataV2<CreateAccessToken, PaymentFlowData, AccessTokenRequestData, AccessTokenResponseData>, T>>
+    for requests::JpmorganTokenRequest
+{
+    type Error = Error;
+    fn try_from(
+        _item: JpmorganRouterData<RouterDataV2<CreateAccessToken, PaymentFlowData, AccessTokenRequestData, AccessTokenResponseData>, T>,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            grant_type: String::from("client_credentials"),
+            scope: String::from("jpm:payments:sandbox"),
+        })
+    }
+}
+
+impl<F> TryFrom<ResponseRouterData<responses::JpmorganAuthUpdateResponse, Self>>
+    for RouterDataV2<F, PaymentFlowData, AccessTokenRequestData, AccessTokenResponseData>
+{
+    type Error = Error;
+    fn try_from(
+        item: ResponseRouterData<responses::JpmorganAuthUpdateResponse, Self>,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            response: Ok(AccessTokenResponseData {
+                access_token: item.response.access_token.peek().to_string(),
+                token_type: Some(item.response.token_type.clone()),
+                expires_in: Some(item.response.expires_in),
+            }),
+            ..item.router_data
+        })
+    }
+}
+
+fn map_capture_method(
+    capture_method: Option<CaptureMethod>,
+) -> Result<requests::CapMethod, error_stack::Report<errors::ConnectorError>> {
+    match capture_method {
+        Some(CaptureMethod::Automatic) | None => Ok(requests::CapMethod::Now),
+        Some(CaptureMethod::Manual) => Ok(requests::CapMethod::Manual),
+        Some(CaptureMethod::Scheduled)
+        | Some(CaptureMethod::ManualMultiple)
+        | Some(CaptureMethod::SequentialAutomatic) => {
+            Err(errors::ConnectorError::NotImplemented("Capture Method".to_string()).into())
+        }
+    }
+}
+
+impl<T: PaymentMethodDataTypes + std::fmt::Debug + std::marker::Sync + std::marker::Send + 'static + Serialize> TryFrom<JpmorganRouterData<RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>, T>>
+    for requests::JpmorganPaymentsRequest<T>
+{
+    type Error = Error;
+    fn try_from(
+        item: JpmorganRouterData<RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>, T>,
+    ) -> Result<Self, Self::Error> {
+        let router_data = &item.router_data;
+
+        // JPMorgan doesn't support 3DS payments
+        if router_data.resource_common_data.auth_type == common_enums::AuthenticationType::ThreeDs {
+            return Err(errors::ConnectorError::NotSupported {
+                message: "3DS payments".to_string(),
+                connector: "JPMorgan",
+            }
+            .into());
+        }
+
+        match &router_data.request.payment_method_data {
+            PaymentMethodData::Card(card_data) => {
+                let capture_method = map_capture_method(router_data.request.capture_method)?;
+
+                let merchant_software = requests::JpmorganMerchantSoftware {
+                    company_name: Secret::new("JPMC".to_string()),
+                    product_name: Secret::new("Hyperswitch".to_string()),
+                };
+
+                let expiry = requests::Expiry {
+                    month: Secret::new(
+                        card_data
+                            .card_exp_month
+                            .peek()
+                            .parse::<i32>()
+                            .change_context(errors::ConnectorError::RequestEncodingFailed)?,
+                    ),
+                    year: Secret::new(
+                        card_data
+                            .get_expiry_year_4_digit()
+                            .peek()
+                            .parse::<i32>()
+                            .change_context(errors::ConnectorError::RequestEncodingFailed)?,
+                    ),
+                };
+
+                let card = requests::JpmorganCard {
+                    account_number: card_data.card_number.clone(),
+                    expiry,
+                };
+
+                let payment_method_type = requests::JpmorganPaymentMethodType { card };
+
+                Ok(Self {
+                    capture_method,
+                    currency: router_data.request.currency,
+                    amount: router_data.request.minor_amount,
+                    merchant: requests::JpmorganMerchant { merchant_software },
+                    payment_method_type,
+                })
+            }
+            _ => Err(errors::ConnectorError::NotImplemented(
+                "Payment method not supported".to_string(),
+            )
+            .into()),
+        }
+    }
+}
+
+impl<T: PaymentMethodDataTypes + std::fmt::Debug + std::marker::Sync + std::marker::Send + 'static + Serialize> TryFrom<JpmorganRouterData<RouterDataV2<Capture, PaymentFlowData, PaymentsCaptureData, PaymentsResponseData>, T>>
+    for requests::JpmorganCaptureRequest
+{
+    type Error = Error;
+    fn try_from(
+        item: JpmorganRouterData<RouterDataV2<Capture, PaymentFlowData, PaymentsCaptureData, PaymentsResponseData>, T>,
+    ) -> Result<Self, Self::Error> {
+        let capture_method = Some(map_capture_method(item.router_data.request.capture_method)?);
+        Ok(Self {
+            capture_method,
+            amount: item.router_data.request.minor_amount_to_capture,
+            currency: Some(item.router_data.request.currency),
+        })
+    }
+}
+
+impl<T: PaymentMethodDataTypes + std::fmt::Debug + std::marker::Sync + std::marker::Send + 'static + Serialize> TryFrom<JpmorganRouterData<RouterDataV2<Void, PaymentFlowData, PaymentVoidData, PaymentsResponseData>, T>>
+    for requests::JpmorganVoidRequest
+{
+    type Error = Error;
+    fn try_from(
+        item: JpmorganRouterData<RouterDataV2<Void, PaymentFlowData, PaymentVoidData, PaymentsResponseData>, T>,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            amount: item.router_data.request.amount,
+            is_void: Some(true),
+            reversal_reason: item.router_data.request.cancellation_reason.as_ref().map(|reason| {
+                reason.parse::<requests::ReversalReason>()
+            }).transpose().ok().flatten(),
+        })
+    }
+}
+
+impl<T: PaymentMethodDataTypes + std::fmt::Debug + std::marker::Sync + std::marker::Send + 'static + Serialize> TryFrom<JpmorganRouterData<RouterDataV2<Refund, RefundFlowData, RefundsData, RefundsResponseData>, T>>
+    for requests::JpmorganRefundRequest
+{
+    type Error = Error;
+    fn try_from(
+        item: JpmorganRouterData<RouterDataV2<Refund, RefundFlowData, RefundsData, RefundsResponseData>, T>,
+    ) -> Result<Self, Self::Error> {
+        let merchant_software = requests::JpmorganMerchantSoftware {
+            company_name: Secret::new("JPMC".to_string()),
+            product_name: Secret::new("Hyperswitch".to_string()),
+        };
+        let merchant = requests::JpmorganMerchantRefund { merchant_software };
+
+        Ok(Self {
+            merchant,
+            amount: item.router_data.request.minor_refund_amount,
+            currency: item.router_data.request.currency,
+        })
+    }
+}
+
+fn map_transaction_state_to_attempt_status(
+    transaction_state: &responses::JpmorganTransactionState,
+    capture_method: &Option<requests::CapMethod>,
+) -> AttemptStatus {
+    match transaction_state {
+        responses::JpmorganTransactionState::Closed => match capture_method {
+            Some(requests::CapMethod::Now) => AttemptStatus::Charged,
+            _ => AttemptStatus::Authorized,
+        },
+        responses::JpmorganTransactionState::Authorized => AttemptStatus::Authorized,
+        responses::JpmorganTransactionState::Declined | responses::JpmorganTransactionState::Error => {
+            AttemptStatus::Failure
+        }
+        responses::JpmorganTransactionState::Pending => AttemptStatus::Pending,
+        responses::JpmorganTransactionState::Voided => AttemptStatus::Voided,
+    }
+}
+
+impl TryFrom<&responses::JpmorganPaymentsResponse> for PaymentsResponseData {
+    type Error = Error;
+    fn try_from(item: &responses::JpmorganPaymentsResponse) -> Result<Self, Self::Error> {
+        Ok(PaymentsResponseData::TransactionResponse {
+            resource_id: ResponseId::ConnectorTransactionId(item.transaction_id.clone()),
+            redirection_data: None,
+            mandate_reference: None,
+            connector_metadata: None,
+            network_txn_id: None,
+            connector_response_reference_id: Some(item.request_id.clone()),
+            incremental_authorization_allowed: None,
+            status_code: item.response_code.parse::<u16>().unwrap_or(0),
+        })
+    }
+}
+
+impl TryFrom<&responses::JpmorganPaymentsResponse> for AttemptStatus {
+    type Error = Error;
+    fn try_from(item: &responses::JpmorganPaymentsResponse) -> Result<Self, Self::Error> {
+        Ok(map_transaction_state_to_attempt_status(&item.transaction_state, &item.capture_method))
+    }
+}
+
+impl TryFrom<&responses::JpmorganRefundResponse> for RefundsResponseData {
+    type Error = Error;
+    fn try_from(item: &responses::JpmorganRefundResponse) -> Result<Self, Self::Error> {
+        let refund_status = responses::RefundStatus::from((
+            item.response_status.clone(),
+            item.transaction_state.clone(),
+        ))
+        .into();
+
+        Ok(RefundsResponseData {
+            connector_refund_id: item.transaction_id.clone(),
+            refund_status,
+            status_code: item.response_code.parse::<u16>().unwrap_or(0),
+        })
+    }
+}
+
+// Bridge pattern implementations for RouterDataV2
+
+impl<T: PaymentMethodDataTypes, F> TryFrom<ResponseRouterData<responses::JpmorganPaymentsResponse, Self>>
+    for RouterDataV2<F, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>
+{
+    type Error = Error;
+    fn try_from(
+        item: ResponseRouterData<responses::JpmorganPaymentsResponse, Self>,
+    ) -> Result<Self, Self::Error> {
+        let status = AttemptStatus::try_from(&item.response)?;
+        let response_data = PaymentsResponseData::try_from(&item.response)?;
+
+        Ok(Self {
+            response: Ok(response_data),
+            resource_common_data: PaymentFlowData {
+                status,
+                ..item.router_data.resource_common_data
+            },
+            ..item.router_data
+        })
+    }
+}
+
+impl<F> TryFrom<ResponseRouterData<responses::JpmorganPaymentsResponse, Self>>
+    for RouterDataV2<F, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>
+{
+    type Error = Error;
+    fn try_from(
+        item: ResponseRouterData<responses::JpmorganPaymentsResponse, Self>,
+    ) -> Result<Self, Self::Error> {
+        let status = AttemptStatus::try_from(&item.response)?;
+        let response_data = PaymentsResponseData::try_from(&item.response)?;
+
+        Ok(Self {
+            response: Ok(response_data),
+            resource_common_data: PaymentFlowData {
+                status,
+                ..item.router_data.resource_common_data
+            },
+            ..item.router_data
+        })
+    }
+}
+
+impl<F> TryFrom<ResponseRouterData<responses::JpmorganPaymentsResponse, Self>>
+    for RouterDataV2<F, PaymentFlowData, PaymentsCaptureData, PaymentsResponseData>
+{
+    type Error = Error;
+    fn try_from(
+        item: ResponseRouterData<responses::JpmorganPaymentsResponse, Self>,
+    ) -> Result<Self, Self::Error> {
+        let status = AttemptStatus::try_from(&item.response)?;
+        let response_data = PaymentsResponseData::try_from(&item.response)?;
+
+        Ok(Self {
+            response: Ok(response_data),
+            resource_common_data: PaymentFlowData {
+                status,
+                ..item.router_data.resource_common_data
+            },
+            ..item.router_data
+        })
+    }
+}
+
+impl<F> TryFrom<ResponseRouterData<responses::JpmorganPaymentsResponse, Self>>
+    for RouterDataV2<F, PaymentFlowData, PaymentVoidData, PaymentsResponseData>
+{
+    type Error = Error;
+    fn try_from(
+        item: ResponseRouterData<responses::JpmorganPaymentsResponse, Self>,
+    ) -> Result<Self, Self::Error> {
+        let status = AttemptStatus::try_from(&item.response)?;
+        let response_data = PaymentsResponseData::try_from(&item.response)?;
+
+        Ok(Self {
+            response: Ok(response_data),
+            resource_common_data: PaymentFlowData {
+                status,
+                ..item.router_data.resource_common_data
+            },
+            ..item.router_data
+        })
+    }
+}
+
+impl<F> TryFrom<ResponseRouterData<responses::JpmorganRefundResponse, Self>>
+    for RouterDataV2<F, RefundFlowData, RefundsData, RefundsResponseData>
+{
+    type Error = Error;
+    fn try_from(
+        item: ResponseRouterData<responses::JpmorganRefundResponse, Self>,
+    ) -> Result<Self, Self::Error> {
+        let status = responses::RefundStatus::from((
+            item.response.response_status.clone(),
+            item.response.transaction_state.clone(),
+        ))
+        .into();
+        let response_data = RefundsResponseData::try_from(&item.response)?;
+
+        Ok(Self {
+            response: Ok(response_data),
+            resource_common_data: RefundFlowData {
+                status,
+                ..item.router_data.resource_common_data
+            },
+            ..item.router_data
+        })
+    }
+}
+
+impl<F> TryFrom<ResponseRouterData<responses::JpmorganRefundResponse, Self>>
+    for RouterDataV2<F, RefundFlowData, RefundSyncData, RefundsResponseData>
+{
+    type Error = Error;
+    fn try_from(
+        item: ResponseRouterData<responses::JpmorganRefundResponse, Self>,
+    ) -> Result<Self, Self::Error> {
+        let status = responses::RefundStatus::from((
+            item.response.response_status.clone(),
+            item.response.transaction_state.clone(),
+        ))
+        .into();
+        let response_data = RefundsResponseData::try_from(&item.response)?;
+
+        Ok(Self {
+            response: Ok(response_data),
+            resource_common_data: RefundFlowData {
+                status,
+                ..item.router_data.resource_common_data
+            },
+            ..item.router_data
+        })
+    }
+}
+
