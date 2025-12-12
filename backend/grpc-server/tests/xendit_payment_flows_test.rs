2ff8942fa9b803460ef754c6ec7a2599e81b4e4f
Saptak Dutta <saptakd88@gmail.com>
2025-12-12 11:43:08 +0530

    fix xendit test file

--- backend/grpc-server/tests/xendit_payment_flows_test.rs
+++ backend/grpc-server/tests/xendit_payment_flows_test.rs
@@ -128,7 +128,15 @@ fn create_authorize_request(capture_method: CaptureMethod) -> PaymentServiceAuth
             "http://localhost:8080/webhooks/merchant_1753672298/mca_8rIwEeXmFvrIA59fMH75".to_string(),
         ),
         email: Some(TEST_EMAIL.to_string().into()),
-        address: Some(grpc_api_types::payments::PaymentAddress::default()),
+        address: Some(grpc_api_types::payments::PaymentAddress {
+            billing_address: Some(grpc_api_types::payments::Address {
+                phone_number: Some(Secret::new("9123456789".to_string())),
+                phone_country_code: Some("+91".to_string()),
+                email: Some(Secret::new("kalo@hul.com".to_string())),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }),
         auth_type: i32::from(AuthenticationType::NoThreeDs),
         request_ref_id: Some(Identifier {
             id_type: Some(IdType::Id(TEST_REQUEST_REF_ID.to_string())),
