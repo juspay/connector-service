51f8a1c34b48553a2e576249215e0afb8e5155d3
Saptak Dutta <saptakd88@gmail.com>
2025-12-23 20:27:23 +0530

    fix formating

--- backend/connector-integration/src/connectors/revolut.rs
+++ backend/connector-integration/src/connectors/revolut.rs
@@ -610,8 +610,12 @@ impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
 }
 
 impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
-    ConnectorIntegrationV2<RepeatPayment, PaymentFlowData, RepeatPaymentData<T>, PaymentsResponseData>
-    for Revolut<T>
+    ConnectorIntegrationV2<
+        RepeatPayment,
+        PaymentFlowData,
+        RepeatPaymentData<T>,
+        PaymentsResponseData,
+    > for Revolut<T>
 {
 }
 
