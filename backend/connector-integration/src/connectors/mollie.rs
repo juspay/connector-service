ee14220671215c864f1e840c2bf4e6060e88c349
Tushar Shukla <shuklatuhsar100@gmail.com>
2026-01-05 17:56:05 +0530

    compilation checks

--- backend/connector-integration/src/connectors/mollie.rs
+++ backend/connector-integration/src/connectors/mollie.rs
@@ -112,7 +112,7 @@ impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
 }
 
 impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
-    connector_types::RepeatPaymentV2 for Mollie<T>
+    connector_types::RepeatPaymentV2<T> for Mollie<T>
 {
 }
 
@@ -628,7 +628,7 @@ impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
 
 // Repeat Payment
 impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
-    ConnectorIntegrationV2<RepeatPayment, PaymentFlowData, RepeatPaymentData, PaymentsResponseData>
+    ConnectorIntegrationV2<RepeatPayment, PaymentFlowData, RepeatPaymentData<T>, PaymentsResponseData>
     for Mollie<T>
 {
 }
@@ -975,7 +975,7 @@ impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
     interfaces::verification::SourceVerification<
         RepeatPayment,
         PaymentFlowData,
-        RepeatPaymentData,
+        RepeatPaymentData<T>,
         PaymentsResponseData,
     > for Mollie<T>
 {
