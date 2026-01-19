4192d063c7e02da60eec0e29c8a4996d16eab335
debarshi.gupta <debarshi.gupta@juspay.in>
2026-01-19 13:25:57 +0530

    merge conflicts resolved

--- backend/connector-integration/src/connectors/getnet.rs
+++ backend/connector-integration/src/connectors/getnet.rs
@@ -263,14 +263,14 @@ macros::create_all_prerequisites!(
             &self,
             req: &'a RouterDataV2<F, PaymentFlowData, Req, Res>,
         ) -> &'a str {
-            &req.resource_common_data.connectors.getnet.base_url
+            &req.resource_common_data.connectors.get_config().getnet.base_url
         }
 
         pub fn connector_base_url_refunds<'a, F, Req, Res>(
             &self,
             req: &'a RouterDataV2<F, RefundFlowData, Req, Res>,
         ) -> &'a str {
-            &req.resource_common_data.connectors.getnet.base_url
+            &req.resource_common_data.connectors.get_config().getnet.base_url
         }
     }
 );
@@ -291,7 +291,7 @@ impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize> Conn
     }
 
     fn base_url<'a>(&self, connectors: &'a Connectors) -> &'a str {
-        &connectors.getnet.base_url
+        &connectors.get_config().getnet.base_url
     }
 
     fn build_error_response(
