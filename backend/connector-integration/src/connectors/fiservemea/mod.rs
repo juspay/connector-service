e2d5385deada18b3101098c5860ca4030c1dbc85
Tushar Shukla <shuklatuhsar100@gmail.com>
2026-02-06 22:47:39 +0530

    Auto-commit changes from tool execution chatcmpl-tool-8d583aaf78ca12c6

--- backend/connector-integration/src/connectors/fiservemea/mod.rs
+++ backend/connector-integration/src/connectors/fiservemea/mod.rs
@@ -219,7 +219,7 @@ macros::create_all_prerequisites!(
     api: [
         (
             flow: Authorize,
-            request_body: FiservemeaPaymentsRequest,
+            request_body: FiservemeaPaymentsRequest<T>,
             response_body: FiservemeaAuthorizeResponse,
             router_data: RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
         ),
