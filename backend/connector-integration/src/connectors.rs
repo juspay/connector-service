1903140c297f03f58b2461360a98dbe8cfa35896
Tushar Shukla <shuklatuhsar100@gmail.com>
2026-02-09 19:04:14 +0530

    Auto-commit changes from tool execution chatcmpl-tool-975959029035c522

--- backend/connector-integration/src/connectors.rs
+++ backend/connector-integration/src/connectors.rs
@@ -8,8 +8,8 @@ pub mod fiservema;
 pub mod razorpayv2;
 
 pub use self::{
-    adyen::Adyen, authorizedotnet::Authorizedotnet, fiserv::Fiserv, mifinity::Mifinity,
-    razorpay::Razorpay, razorpayv2::RazorpayV2,
+    adyen::Adyen, authorizedotnet::Authorizedotnet, fiserv::Fiserv, fiservema::Fiservema,
+    mifinity::Mifinity, razorpay::Razorpay, razorpayv2::RazorpayV2,
 };
 
 pub mod elavon;
