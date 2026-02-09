f2e6046aa71e813b25ba6aaa86cd5143fd1a1e9c
Tushar Shukla <shuklatuhsar100@gmail.com>
2026-02-09 20:07:30 +0530

    Auto-commit changes from tool execution chatcmpl-tool-b6ecd970c2c568c4

--- backend/connector-integration/src/connectors/fiservemea/transformers.rs
+++ backend/connector-integration/src/connectors/fiservemea/transformers.rs
@@ -288,29 +288,25 @@ impl ForeignTryFrom<RouterDataV2<RSync, RefundFlowData, RefundSyncData, RefundsR
     }
 }
 
-impl<F> TryFrom<RouterDataV2<F, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>>
+impl TryFrom<RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>>
     for FiservEMEASyncRequest
-where
-    F: Clone,
 {
     type Error = error_stack::Report<ConnectorError>;
 
     fn try_from(
-        _item: RouterDataV2<F, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>,
+        _item: RouterDataV2<PSync, PaymentFlowData, PaymentsSyncData, PaymentsResponseData>,
     ) -> Result<Self, Self::Error> {
         Ok(Self {})
     }
 }
 
-impl<F> TryFrom<RouterDataV2<F, RefundFlowData, RefundSyncData, RefundsResponseData>>
+impl TryFrom<RouterDataV2<RSync, RefundFlowData, RefundSyncData, RefundsResponseData>>
     for FiservEMEASyncRequest
-where
-    F: Clone,
 {
     type Error = error_stack::Report<ConnectorError>;
 
     fn try_from(
-        _item: RouterDataV2<F, RefundFlowData, RefundSyncData, RefundsResponseData>,
+        _item: RouterDataV2<RSync, RefundFlowData, RefundSyncData, RefundsResponseData>,
     ) -> Result<Self, Self::Error> {
         Ok(Self {})
     }
