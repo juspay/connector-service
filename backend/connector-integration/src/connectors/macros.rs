fba0139a0e34f6a55134e1f1415a16d7cd7200e6
yashasvi.kapil <yashasvi.kapil@juspay.in>
2026-02-13 00:05:32 +0530

    Auto-commit changes from tool execution chatcmpl-tool-9c2bcc13bcc8b009

--- backend/connector-integration/src/connectors/macros.rs
+++ backend/connector-integration/src/connectors/macros.rs
@@ -875,7 +875,7 @@ macro_rules! impl_templating_mixed {
     // Pattern for non-generic request with XML response parsing
     (
         connector: $connector: ident,
-        curl_request: $base_req: ident,
+        curl_request: $base_req: ty,
         curl_response: $curl_res: ident,
         router_data: $router_data: ty,
         generic_type: $generic_type: tt,
