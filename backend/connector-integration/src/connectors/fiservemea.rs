01c7bb96573749dcf76cfba1c7cbfafc2347daed
yashasvi.kapil <yashasvi.kapil@juspay.in>
2026-02-13 14:01:39 +0530

    Auto-commit changes from tool execution chatcmpl-tool-a1b3857565f9912e

--- backend/connector-integration/src/connectors/fiservemea.rs
+++ backend/connector-integration/src/connectors/fiservemea.rs
@@ -203,20 +203,20 @@ impl<T: PaymentMethodDataTypes + Debug + Sync + Send + 'static + Serialize>
 }
 
 macros::create_all_prerequisites!(
-    Fiservemea,
-    T,
-    [
+    connector_name: Fiservemea,
+    generic_type: T,
+    api: [
         (
-            Authorize,
-            fiservemea::FiservemeaAuthorizeRequest<T>,
-            fiservemea::FiservemeaAuthorizeResponse,
-            RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
+            flow: Authorize,
+            request_body: fiservemea::FiservemeaAuthorizeRequest<T>,
+            response_body: fiservemea::FiservemeaAuthorizeResponse,
+            router_data: RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
         ),
     ],
-    [
-        StringMajorUnit
+    amount_converters: [
+        amount_converter: StringMajorUnit
     ],
-    {
+    member_functions: {
         pub fn build_headers<F, FCD, Req, Res>(
             &self,
             req: &RouterDataV2<F, FCD, Req, Res>,
