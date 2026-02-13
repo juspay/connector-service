854b567cc9da09245b5388ef24bc74cc046fdedb
yashasvi.kapil <yashasvi.kapil@juspay.in>
2026-02-13 18:36:13 +0530

    Auto-commit changes from tool execution chatcmpl-tool-9e082986ceea0395

--- backend/connector-integration/src/connectors/fiservemea/transformers.rs
+++ backend/connector-integration/src/connectors/fiservemea/transformers.rs
@@ -185,10 +185,7 @@ impl<T: PaymentMethodDataTypes + std::fmt::Debug + Sync + Send + 'static + Seria
             .change_context(errors::ConnectorError::RequestEncodingFailed)?;
 
         let order = Some(FiservemeaOrder {
-            order_id: item
-                .resource_common_data
-                .connector_request_reference_id
-                .clone(),
+            order_id: item.resource_common_data.connector_request_reference_id.clone(),
         });
 
         Ok(Self {
