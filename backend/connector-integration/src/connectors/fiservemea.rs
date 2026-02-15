7ba0815fa4b7783bf5a04a85032f3cf6ead4f00d
Tushar Shukla <tushar.shukla@juspay.in>
2026-02-15 18:29:37 +0530

    Auto-commit changes from tool execution chatcmpl-tool-a63c678c538248b4

--- backend/connector-integration/src/connectors/fiservemea.rs
+++ backend/connector-integration/src/connectors/fiservemea.rs
@@ -42,7 +42,7 @@ macros::create_all_prerequisites!(
     api: [
         (
             flow: Authorize,
-            request_body: fiservemea::FiservemeaAuthorizeRequest<T>,
+            request_body: fiservemea::FiservemeaAuthorizeRequest,
             response_body: fiservemea::FiservemeaAuthorizeResponse,
             router_data: RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
         ),
@@ -84,7 +84,7 @@ macros::create_all_prerequisites!(
 macros::macro_connector_implementation!(
     connector_default_implementations: [get_content_type, get_error_response_v2],
     connector: Fiservemea,
-    curl_request: Json(fiservemea::FiservemeaAuthorizeRequest<T>),
+    curl_request: Json(fiservemea::FiservemeaAuthorizeRequest),
     curl_response: fiservemea::FiservemeaAuthorizeResponse,
     flow_name: Authorize,
     resource_common_data: PaymentFlowData,
