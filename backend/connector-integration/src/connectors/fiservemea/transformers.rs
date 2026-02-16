4f2b88ae45dfa88e6756b918fc217d4f96bef840
Tushar Shukla <tushar.shukla@juspay.in>
2026-02-16 13:40:22 +0530

    Auto-commit changes from tool execution chatcmpl-tool-b7371cacb9f9b784

--- backend/connector-integration/src/connectors/fiservemea/transformers.rs
+++ backend/connector-integration/src/connectors/fiservemea/transformers.rs
@@ -188,7 +188,7 @@ impl<T: PaymentMethodDataTypes + std::fmt::Debug + Sync + Send + 'static + Seria
             >,
             T,
         >,
-    > for FiservemeaAuthorizeRequest<T>
+    > for FiservemeaAuthorizeRequest
 {
     type Error = error_stack::Report<errors::ConnectorError>;
 
