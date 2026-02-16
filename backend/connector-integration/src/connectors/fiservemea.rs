37118b7e0a4ec83f336b51e8c03bbc85f3934622
yashasvi.kapil <yashasvi.kapil@juspay.in>
2026-02-16 15:25:57 +0530

    Auto-commit changes from tool execution chatcmpl-tool-b5e14cf52b6be00c

--- backend/connector-integration/src/connectors/fiservemea.rs
+++ backend/connector-integration/src/connectors/fiservemea.rs
@@ -49,7 +49,7 @@ macros::create_all_prerequisites!(
     api: [
         (
             flow: Authorize,
-            request_body: fiservemea::FiservemeaAuthorizeRequest<T>,
+            request_body: fiservemea::FiservemeaAuthorizeRequest,
             response_body: fiservemea::FiservemeaAuthorizeResponse,
             router_data: RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
         ),
