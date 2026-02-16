fd5951df5edc5631c3e5a81f34d72449566b2de9
PiX <69745008+pixincreate@users.noreply.github.com>
2026-02-16 22:27:01 +0530

    hardcode payment status to `authenticatino_pending` since trustpay does not send one

--- backend/connector-integration/src/connectors/trustpay/transformers.rs
+++ backend/connector-integration/src/connectors/trustpay/transformers.rs
@@ -2183,7 +2183,10 @@ pub(crate) fn get_apple_pay_session(
 
     Ok(RouterDataV2 {
         flow: std::marker::PhantomData,
-        resource_common_data: item.router_data.resource_common_data.clone(),
+        resource_common_data: PaymentFlowData {
+            status: enums::AttemptStatus::AuthenticationPending,
+            ..item.router_data.resource_common_data
+        },
         connector_auth_type: item.router_data.connector_auth_type.clone(),
         request: item.router_data.request.clone(),
         response: Ok(PaymentCreateOrderResponse {
@@ -2237,7 +2240,10 @@ pub(crate) fn get_google_pay_session(
 
     Ok(RouterDataV2 {
         flow: std::marker::PhantomData,
-        resource_common_data: item.router_data.resource_common_data.clone(),
+        resource_common_data: PaymentFlowData {
+            status: enums::AttemptStatus::AuthenticationPending,
+            ..item.router_data.resource_common_data
+        },
         connector_auth_type: item.router_data.connector_auth_type.clone(),
         request: item.router_data.request.clone(),
         response: Ok(PaymentCreateOrderResponse {
