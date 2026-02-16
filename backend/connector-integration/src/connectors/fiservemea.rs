2f31dec75173d9e35ebcad447649609c463748f0
Tushar Shukla <tushar.shukla@juspay.in>
2026-02-16 15:46:24 +0530

    Auto-commit changes from tool execution chatcmpl-tool-925718541c7b8caf

--- backend/connector-integration/src/connectors/fiservemea.rs
+++ backend/connector-integration/src/connectors/fiservemea.rs
@@ -5,7 +5,7 @@ use std::fmt::Debug;
 use common_enums::CurrencyUnit;
 use common_utils::{errors::CustomResult, events, ext_traits::ByteSliceExt};
 use domain_types::{
-    connector_flow::Authorize,
+    connector_flow,
     connector_types::{PaymentFlowData, PaymentsAuthorizeData, PaymentsResponseData},
     errors,
     payment_method_data::PaymentMethodDataTypes,
