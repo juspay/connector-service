ddfb2495dea84e9db1709bd3eb7bdc07bda73e9e
Tushar Shukla <tushar.shukla@juspay.in>
2026-02-17 16:12:03 +0530

    Auto-commit changes from tool execution chatcmpl-tool-b4f6057eb96d01ce

--- backend/connector-integration/src/connectors/fiservemea.rs
+++ backend/connector-integration/src/connectors/fiservemea.rs
@@ -51,7 +51,7 @@ macros::create_all_prerequisites!(
             flow: Authorize,
             request_body: FiservemeaAuthorizeRequest<T>,
             response_body: FiservemeaAuthorizeResponse,
-            router_data: RouterData<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
+            router_data: RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
         ),
     ],
     amount_converters: [],
