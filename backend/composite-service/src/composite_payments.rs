84019200ffa5f85dc849783583a378a5b6ae3a42
Amitsingh Tanwar <126856945+AmitsinghTanwar007@users.noreply.github.com>
2026-02-17 17:04:35 +0530

    chore: Added Crate for Composite Flows (#512)
    Co-authored-by: Natarajan Kannan <natarajan@juspay.in>

---
+++ backend/composite-service/src/composite_payments.rs
@@ -0,0 +1,19 @@
+// use connector_integration::types::ConnectorData;
+// use domain_types::{connector_types::ConnectorEnum, utils::ForeignTryFrom as _};
+// use grpc_api_types::payments::{
+//     composite_payment_service_server::CompositePaymentService,
+//     payment_service_server::PaymentService, CompositeAuthorizeRequest, CompositeAuthorizeResponse,
+//     PaymentServiceAuthorizeResponse, PaymentServiceCreateAccessTokenResponse,
+//     PaymentServiceCreateConnectorCustomerResponse,
+// };
+
+#[derive(Clone)]
+pub struct CompositePayments<S> {
+    _payment_service: S,
+}
+
+// impl<S> CompositePayments<S> {
+//     pub fn new(payment_service: S) -> Self {
+//         Self { payment_service }
+//     }
+// }
