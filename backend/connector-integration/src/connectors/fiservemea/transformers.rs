4baf41277152490b9f842e67b5574b5f1f74a751
Tushar Shukla <tushar.shukla@juspay.in>
2026-02-18 15:04:51 +0530

    Auto-commit changes from tool execution chatcmpl-tool-9d0106b9871d6013

--- backend/connector-integration/src/connectors/fiservemea/transformers.rs
+++ backend/connector-integration/src/connectors/fiservemea/transformers.rs
@@ -160,7 +160,7 @@ impl<T: PaymentMethodDataTypes> TryFrom<&RouterDataV2<Authorize, PaymentFlowData
 
         let amount = domain_types::utils::convert_amount(
             &common_utils::types::StringMajorUnitForConnector,
-            item.request.minor_amount.get_amount_as_i64(),
+            MinorUnit(item.request.minor_amount.get_amount_as_i64()),
             item.request.currency,
         )?;
 
