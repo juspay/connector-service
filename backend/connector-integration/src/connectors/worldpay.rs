54692302ec81f06f187dcaa0a5971225a896ba7f
Tushar Shukla <tushar.shukla@juspay.in>
2026-02-20 16:19:13 +0530

    Auto-commit changes from tool execution functions.edit:40

--- backend/connector-integration/src/connectors/worldpay.rs
+++ backend/connector-integration/src/connectors/worldpay.rs
@@ -80,7 +80,7 @@ macros::create_all_prerequisites!(
     api: [
         (
             flow: Authorize,
-            request_body: worldpay::WorldpayAuthorizeRequest<T>,
+            request_body: worldpay::WorldpayAuthorizeRequest,
             response_body: worldpay::WorldpayAuthorizeResponse,
             router_data: RouterDataV2<Authorize, PaymentFlowData, PaymentsAuthorizeData<T>, PaymentsResponseData>,
         ),
