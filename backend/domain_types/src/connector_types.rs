8e7df04f94aa27eb174e701ba80cc0785f595acd
yashasvi.kapil <yashasvi.kapil@juspay.in>
2026-02-20 16:14:52 +0530

    Add connector fiservemea

--- backend/domain_types/src/connector_types.rs
+++ backend/domain_types/src/connector_types.rs
@@ -111,6 +111,7 @@ pub enum ConnectorEnum {
     Loonio,
     Wellsfargo,
     Hyperpg,
+    Fiservemea,
 }
 
 impl ForeignTryFrom<grpc_api_types::payments::Connector> for ConnectorEnum {
@@ -185,6 +186,7 @@ impl ForeignTryFrom<grpc_api_types::payments::Connector> for ConnectorEnum {
             grpc_api_types::payments::Connector::Loonio => Ok(Self::Loonio),
             grpc_api_types::payments::Connector::Wellsfargo => Ok(Self::Wellsfargo),
             grpc_api_types::payments::Connector::Hyperpg => Ok(Self::Hyperpg),
+            grpc_api_types::payments::Connector::Fiservemea => Ok(Self::Fiservemea),
             grpc_api_types::payments::Connector::Unspecified => {
                 Err(ApplicationErrorResponse::BadRequest(ApiError {
                     sub_code: "UNSPECIFIED_CONNECTOR".to_owned(),
