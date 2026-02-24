d0cbacf53856f9352e690aa56b48041487d31712
aadityaguptaa <aditya.gupta.001@juspay.in>
2026-02-24 11:59:00 +0530

    feat: redirect wallets

--- backend/connector-integration/src/connectors/adyen/transformers.rs
+++ backend/connector-integration/src/connectors/adyen/transformers.rs
@@ -257,12 +257,59 @@ pub enum AdyenPaymentMethod<
     Seicomart(Box<JCSVoucherData>),
     #[serde(rename = "econtext_stores")]
     PayEasy(Box<JCSVoucherData>),
+    // Wallet redirect payment methods
+    #[serde(rename = "alipay")]
+    AliPay,
+    #[serde(rename = "alipay_hk")]
+    AliPayHk,
+    #[serde(rename = "dana")]
+    Dana,
+    #[serde(rename = "gcash")]
+    Gcash(Box<GcashData>),
+    #[serde(rename = "gopay_wallet")]
+    GoPay(Box<GoPayData>),
+    #[serde(rename = "kakaopay")]
+    Kakaopay(Box<KakaoPayData>),
+    #[serde(rename = "mbway")]
+    Mbway(Box<MbwayData>),
+    #[serde(rename = "momo_wallet")]
+    Momo(Box<MomoData>),
+    #[serde(rename = "touchngo")]
+    TouchNGo(Box<TouchNGoData>),
+    #[serde(rename = "wechatpayWeb")]
+    WeChatPayWeb,
+    #[serde(rename = "twint")]
+    Twint,
+    #[serde(rename = "vipps")]
+    Vipps,
+    #[serde(rename = "swish")]
+    Swish,
 }
 
 #[derive(Debug, Clone, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BlikRedirectionData {
     blik_code: Secret<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GoPayData {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct KakaoPayData {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GcashData {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MomoData {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TouchNGoData {}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct MbwayData {
+    telephone_number: Secret<String>,
 }
 
 #[derive(Debug, Clone, Serialize)]
@@ -1358,7 +1405,7 @@ impl<T: PaymentMethodDataTypes + std::fmt::Debug + Sync + Send + 'static + Seria
             >,
         ),
     ) -> Result<Self, Self::Error> {
-        let (wallet_data, _item) = value;
+        let (wallet_data, item) = value;
         match wallet_data {
             WalletData::GooglePay(data) => {
                 let gpay_data = AdyenGPay {
@@ -1385,26 +1432,27 @@ impl<T: PaymentMethodDataTypes + std::fmt::Debug + Sync + Send + 'static + Seria
                 };
                 Ok(Self::ApplePay(Box::new(apple_pay_data)))
             }
-
+            WalletData::AliPayRedirect(_) => Ok(Self::AliPay),
+            WalletData::AliPayHkRedirect(_) => Ok(Self::AliPayHk),
+            WalletData::DanaRedirect { .. } => Ok(Self::Dana),
+            WalletData::GcashRedirect(_) => Ok(Self::Gcash(Box::new(GcashData {}))),
+            WalletData::GoPayRedirect(_) => Ok(Self::GoPay(Box::new(GoPayData {}))),
+            WalletData::KakaoPayRedirect(_) => Ok(Self::Kakaopay(Box::new(KakaoPayData {}))),
+            WalletData::MbWayRedirect(_) => Ok(Self::Mbway(Box::new(MbwayData {
+                telephone_number: item.resource_common_data.get_billing_phone_number()?,
+            }))),
+            WalletData::MomoRedirect(_) => Ok(Self::Momo(Box::new(MomoData {}))),
+            WalletData::TouchNGoRedirect(_) => Ok(Self::TouchNGo(Box::new(TouchNGoData {}))),
+            WalletData::WeChatPayRedirect(_) => Ok(Self::WeChatPayWeb),
+            WalletData::TwintRedirect { .. } => Ok(Self::Twint),
+            WalletData::VippsRedirect { .. } => Ok(Self::Vipps),
+            WalletData::SwishQr(_) => Ok(Self::Swish),
             WalletData::PaypalRedirect(_)
             | WalletData::AmazonPayRedirect(_)
             | WalletData::Paze(_)
             | WalletData::RevolutPay(_)
-            | WalletData::AliPayRedirect(_)
-            | WalletData::AliPayHkRedirect(_)
-            | WalletData::GoPayRedirect(_)
-            | WalletData::KakaoPayRedirect(_)
-            | WalletData::GcashRedirect(_)
-            | WalletData::MomoRedirect(_)
-            | WalletData::TouchNGoRedirect(_)
-            | WalletData::MbWayRedirect(_)
             | WalletData::MobilePayRedirect(_)
-            | WalletData::WeChatPayRedirect(_)
             | WalletData::SamsungPay(_)
-            | WalletData::TwintRedirect { .. }
-            | WalletData::VippsRedirect { .. }
-            | WalletData::DanaRedirect { .. }
-            | WalletData::SwishQr(_)
             | WalletData::AliPayQr(_)
             | WalletData::ApplePayRedirect(_)
             | WalletData::ApplePayThirdPartySdk(_)
