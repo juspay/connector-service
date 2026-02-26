d9d1b14509174dc22255230b0a8cacc06d8fea61
Jeeva0104 <jeeva.ramachandran@juspay.in>
2026-02-26 12:39:55 +0530

    resolve lint

--- backend/grpc-server/src/http/handlers/composite/payments.rs
+++ backend/grpc-server/src/http/handlers/composite/payments.rs
@@ -9,12 +9,12 @@ use grpc_api_types::payments::{
 };
 use std::sync::Arc;
 
-use ucs_env::configs::Config;
 use crate::http::handlers::macros::http_handler;
 use crate::http::{
     error::HttpError, http_headers_to_grpc_metadata, state::AppState,
     transfer_config_to_grpc_request, utils::ValidatedJson,
 };
+use ucs_env::configs::Config;
 
 http_handler!(
     authorize,
