daa045bd79cbf314561f8864638b4d2b85e736cd
Jagan <jaganelavarasan@gmail.com>
2026-03-03 23:27:34 +0530

    refactor(compilation): eliminate diesel from FFI dep tree and centralise workspace deps (#573)

--- sdk/javascript/src/index.ts
+++ sdk/javascript/src/index.ts
@@ -1,3 +1,4 @@
-export * from "./payments/connector_client";
-export * from "./payments/uniffi_client";
+export { ConnectorClient } from "./payments/_generated_connector_client_flows";
+export { UniffiClient } from "./payments/_generated_uniffi_client_flows";
+export type { RustBuffer, RustCallStatus } from "./payments/uniffi_client";
 export * from "./http_client";
