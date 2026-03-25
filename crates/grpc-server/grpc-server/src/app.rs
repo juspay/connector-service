f7fc46e23a58fc2acb3ddd302e51e0843e5d7b5c
Shubhodip Pal <palshubhodip9@gmail.com>
2026-03-25 14:48:03 +0530

    removed webhook changes

--- crates/grpc-server/grpc-server/src/app.rs
+++ crates/grpc-server/grpc-server/src/app.rs
@@ -5,7 +5,7 @@ use grpc_api_types::{
     health_check::health_server,
     payments::{
         composite_payment_service_server, composite_refund_service_server, customer_service_server,
-        dispute_service_server, event_service_server, merchant_authentication_service_server,
+        dispute_service_server, merchant_authentication_service_server,
         payment_method_authentication_service_server, payment_method_service_server,
         payment_service_server, recurring_payment_service_server, refund_service_server,
     },
@@ -309,10 +309,6 @@ impl Service {
             ))
             .add_service(payout_service_server::PayoutServiceServer::new(
                 self.payouts_service,
-            ))
-            .add_service(
-                event_service_server::EventServiceServer::new(
-                    self.event_service,
             ))
             .serve_with_shutdown(socket, shutdown_signal)
             .await?;
