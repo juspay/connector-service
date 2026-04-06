6edb68c141d67082f4c64ec8ef1c2e65219f2d21
Jagan <jaganelavarasan@gmail.com>
2026-04-07 01:10:03 +0530

    refactor(client): refactor rust connector client (#939)

--- crates/types-traits/domain_types/src/types.rs
+++ crates/types-traits/domain_types/src/types.rs
@@ -12019,7 +12019,10 @@ pub fn generate_mandate_revoke_response(
         MandateRevokeRequestData,
         connector_types::MandateRevokeResponseData,
     >,
-) -> Result<RecurringPaymentServiceRevokeResponse, error_stack::Report<IntegrationError>> {
+) -> Result<
+    RecurringPaymentServiceRevokeResponse,
+    error_stack::Report<ConnectorResponseTransformationError>,
+> {
     let mandate_revoke_response = router_data_v2.response;
     let raw_connector_response = router_data_v2
         .resource_common_data
