03e9fab77a23076451fc29248a8858d6d24f94c3
Uzair Khan <29498864+maverox@users.noreply.github.com>
2026-04-08 18:19:42 +0530

    feat(shared): consolidate ClientAuthenticationToken shared types for all connectors (#1002)

--- crates/types-traits/domain_types/src/payment_method_data.rs
+++ crates/types-traits/domain_types/src/payment_method_data.rs
@@ -379,6 +379,7 @@ pub struct GiftCardDetails {
 }
 
 // TODO: Add payment method token field and also rename the struct to PaymentMethodToken since it is not being used anywhere
+// TODO: Add payment method token field and also rename the struct to PaymentMethodToken since it is not being used anywhere
 #[derive(Eq, PartialEq, Debug, serde::Deserialize, serde::Serialize, Clone, Default)]
 #[serde(rename_all = "snake_case")]
 pub struct CardToken {
